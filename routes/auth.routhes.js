b33313a79d6ce53d986572d6cfd77a328d9dd46f
JanuaryLunR <91497281+JanuaryLunR@users.noreply.github.com>
2022-01-12 21:33:05 +0300

    Day 1
    Connect MongoDB and Config

---
+++ routes/auth.routhes.js
@@ -0,0 +1,18 @@
+const {} = require('express')
+const router = Router()
+
+// /api/auth+
+router.post('/register', async (req, res) => {
+  try {
+    
+  } catch (e) {
+
+  }
+})
+
+// /api/auth+
+router.post('/login', async (req, res) => {
+
+})
+
+module.exports = router
